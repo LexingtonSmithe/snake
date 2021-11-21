d0848817a7e4213c5c96d719d3fd18000714357f
LexingtonSmithe <lowndesy_3@hotmail.com>
2021-11-21 20:07:51 +0000

    init

---
+++ sketch.js
@@ -0,0 +1,71 @@
+// Daniel Shiffman
+// http://youtube.com/thecodingtrain
+// http://codingtra.in
+
+// Coding Challenge #115: Snake Game Redux
+// https://youtu.be/OMoVcohRgZA
+
+let snake;
+let rez = 100;
+let food;
+let w;
+let h;
+let speed = 5;
+let imgB, imgN;
+
+function preload(){
+  imgB = loadImage('/assets/bakey.jpg');
+  imgN = loadImage('/assets/nugget.jpg');
+}
+
+function setup() {
+  createCanvas(windowWidth -50, windowHeight-50);
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(speed);
+  snake = new Snake();
+  foodLocation();
+}
+
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+    snake.setDir(0, -1);
+  } else if (key == ' ') {
+    snake.grow();
+  }
+
+}
+
+function draw() {
+  scale(rez);
+  background(255);
+  if (snake.eat(food)) {
+    foodLocation();
+  }
+  snake.update();
+  snake.show();
+
+
+  if (snake.endGame()) {
+    print("END GAME");
+    background(255, 0, 0);
+    noLoop();
+  }
+
+  noStroke();
+  fill(255, 0, 0);
+  image(imgN, food.x, food.y, 1, 1);
+}
